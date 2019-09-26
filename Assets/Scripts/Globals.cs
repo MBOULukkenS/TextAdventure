7aeb667dc991c9524bb69e8fe6f08a79d0f0b58e
Stijn Lukken <326523@student.mboutrecht.nl>
2019-09-26 20:30:54 +0200

    Voeg KeyCode constanten toe aan Globals

--- Assets/Scripts/Globals.cs
+++ Assets/Scripts/Globals.cs
@@ -1,7 +1,14 @@
+using UnityEngine;
+
 namespace DefaultNamespace
 {
     public static class Globals
     {
         public static char[] Separator { get; set; } = {' '};
+
+        public const KeyCode SkipKey = KeyCode.Escape;
+        
+        public const KeyCode HistoryPreviousKey = KeyCode.UpArrow;
+        public const KeyCode HistoryNextKey = KeyCode.DownArrow;
     }
 }
