6ffc5445d87a2cac756165f3c4aafed2b7493cf0
Stijn Lukken <326523@student.mboutrecht.nl>
2019-09-26 18:16:29 +0200

    Update Printcommand zodat het Aliases gebruikt

--- Assets/Scripts/TerminalUI/Commands/PrintCommand.cs
+++ Assets/Scripts/TerminalUI/Commands/PrintCommand.cs
@@ -1,15 +1,26 @@
 using System.Collections;
+using TerminalUI.Commands.Base;
 using WM2000.Terminal;
+using Yarn;
 
 namespace TerminalUI.Commands
 {
+    [Alias("PrintLine", "Write", "WriteLine")]
     public class PrintCommand : Command
     {
         public override IEnumerator Run(params string[] args)
         {
-            if (args.Length == 1)
-                Terminal.WriteLine(Wm2000VariableStorage.Instance.GetValue(args[0]).AsString);
-            yield return null;
+            foreach (string arg in args)
+            {
+                Value result = Wm2000VariableStorage.Instance.GetValue(arg);
+
+                Terminal.Write(result.type == Value.Type.Null ? $"{arg} " : result.AsString);
+            }
+            
+            if (args.Length > 0)
+                Terminal.WriteLine();
+
+            yield break;
         }
     }
 }
